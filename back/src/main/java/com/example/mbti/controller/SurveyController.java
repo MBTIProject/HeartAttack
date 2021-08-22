f8093686f199ce10f35124f90ea289d80d53fcf3
alisyabob <sjs939883@gmail.com>
2021-08-22 17:35:50 +0900

    Add 심리테스트 선택지, 결과 조회

--- back/src/main/java/com/example/mbti/controller/SurveyController.java
+++ back/src/main/java/com/example/mbti/controller/SurveyController.java
@@ -4,9 +4,12 @@ import com.example.mbti.dto.SurveyRequestDto;
 import com.example.mbti.model.Survey;
 import com.example.mbti.repository.SurveyRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,8 +18,14 @@ public class SurveyController {
 
     //심리테스트 선택지, 결과 등록
     @PostMapping("survey/list")
-    public Survey createSurvey(@RequestBody SurveyRequestDto surveyRequestDto){
+    public Survey createSurvey(@RequestBody SurveyRequestDto surveyRequestDto) {
         Survey survey = new Survey(surveyRequestDto);
         return surveyRepository.save(survey);
     }
+    //심리테스트 선택지, 결과 조회
+    @GetMapping("survey/list")
+    public List<Survey> selectPost() {
+        return surveyRepository.findAll();
+    }
+
 }
