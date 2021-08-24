98ebc505a029c2f8753f8b116da53c8f69df80c5
alisyabob <sjs939883@gmail.com>
2021-08-25 00:28:05 +0900

    Add sequence

--- back/src/main/java/com/example/mbti/model/Comment.java
+++ back/src/main/java/com/example/mbti/model/Comment.java
@@ -10,10 +10,15 @@ import javax.persistence.*;
 @Getter
 @Entity
 @Setter
+@SequenceGenerator(
+        name = "COMMENT",
+        sequenceName = "COMMENT_SEQ", // 매핑할 데이터베이스 시퀀스 이름
+        initialValue = 1,
+        allocationSize = 1)
 @NamedQuery(name ="Comment.findByPoster_id", query="select c from Comment c where c.poster_id=:poster_id")
 public class Comment extends Timestamped {
 
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "COMMENT")
     @Id
     private Long id;
 
