37bac3c0534208b11ef899ba3f665035e96c54d8
alisyabob <sjs939883@gmail.com>
2021-08-27 20:48:23 +0900

    update Date Format

--- back/src/main/java/com/example/mbti/model/Timestamped.java
+++ back/src/main/java/com/example/mbti/model/Timestamped.java
@@ -1,13 +1,16 @@
 package com.example.mbti.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @MappedSuperclass // Entity가 자동으로 컬럼으로 인식합니다.
 @EntityListeners(AuditingEntityListener.class) // 생성/변경 시간을 자동으로 업데이트합니다.
@@ -15,5 +18,5 @@ import java.time.LocalDateTime;
 public abstract class Timestamped {
 
     @CreatedDate
-    private LocalDateTime createdAt;
+    private String createdAt = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm"));
 }
