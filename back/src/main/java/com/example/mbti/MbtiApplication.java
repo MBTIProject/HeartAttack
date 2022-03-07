638dd7119a29be1936f6282573dcafde6d9dc6a3
alisyabob <sjs939883@gmail.com>
2022-03-08 03:18:05 +0900

    [CHORE] queryDsl 세팅

--- back/src/main/java/com/example/mbti/MbtiApplication.java
+++ back/src/main/java/com/example/mbti/MbtiApplication.java
@@ -1,8 +1,12 @@
 package com.example.mbti;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import javax.persistence.EntityManager;
 
 @EnableJpaAuditing
 @SpringBootApplication
@@ -10,6 +14,11 @@ public class MbtiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MbtiApplication.class, args);
+    }
+
+    @Bean
+    JPAQueryFactory jpaQueryFactory(EntityManager entityManager) {
+        return new JPAQueryFactory(entityManager);
     }
 
 }
