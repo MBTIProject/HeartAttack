0469dce0e0630cc41ccfdae10c8cb990a96ba5b0
alisyabob <sjs939883@gmail.com>
2022-03-08 15:04:04 +0900

    [Refactor] 필드 변경 및 객체간 결합도를 낮추고 유연한개발 및 다형성을 위해 serviceImpl 추가

--- back/src/main/java/com/example/mbti/model/Poster.java
+++ back/src/main/java/com/example/mbti/model/Poster.java
@@ -1,14 +1,11 @@
 package com.example.mbti.model;
 
-import com.example.mbti.dto.PosterRequestDto;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
@@ -28,10 +25,14 @@ public class Poster extends Timestamped  {
     @Column(columnDefinition = "integer default 0")
     private int poster_view_count;
 
+    @Column(nullable = false)
+    private String passage;
+
     @Builder
-    public Poster(String poster_title, String img_url){
+    public Poster(String poster_title, String img_url, String passage){
         this.poster_title = poster_title;
         this.img_url = img_url;
+        this.passage = passage;
     }
 
     public void updatePostCnt() {
---
+++ back/src/main/java/com/example/mbti/service/impl/PosterServiceImpl.java
@@ -0,0 +1,56 @@
+package com.example.mbti.service.impl;
+
+import com.example.mbti.advice.exception.ApiRequestException;
+import com.example.mbti.dto.PosterRequestDto;
+import com.example.mbti.dto.PosterResponseDto;
+import com.example.mbti.model.Poster;
+import com.example.mbti.repository.Poster.PosterRepository;
+import com.example.mbti.service.PosterService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+@Transactional(readOnly = true)
+public class PosterServiceImpl implements PosterService {
+    private final PosterRepository posterRepository;
+
+    @Override
+    @Transactional(readOnly = false)
+    public void addPost(PosterRequestDto posterRequestDto) {
+
+        Optional<Poster> findPosterTitle = posterRepository.findByTitle(posterRequestDto.getPoster_title());
+        if(!findPosterTitle.isPresent()) {
+            Poster poster = Poster.builder()
+                    .poster_title(posterRequestDto.getPoster_title())
+                    .img_url(posterRequestDto.getImg_url())
+                    .passage(posterRequestDto.getPassage())
+                    .build();
+            posterRepository.save(poster);
+        }
+        else {
+            throw new ApiRequestException("동일한 심리테스트 제목이 존재합니다.");
+        }
+    }
+
+    @Override
+    public List<PosterResponseDto> findPost() {
+        return posterRepository.findAll().stream()
+                .map(PosterResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    @Transactional(readOnly = false)
+    public Optional<Poster> modifyPostCnt(Long posterId) {
+        Optional<Poster> findPostId = Optional.ofNullable(posterRepository.findById(posterId).orElseThrow(()
+                -> new ApiRequestException("존재하지 않는 유형입니다.")));
+        findPostId.get().updatePostCnt();
+        return findPostId;
+    }
+}
