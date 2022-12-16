3b49d759e3e5547ddc1bb3d56b49ca7884e86c6f
alisyabob <sjs939883@gmail.com>
2022-12-16 19:29:59 +0900

    [TEST] 제목으로조회 테스트 추가

--- back/src/test/java/com/example/mbti/repository/survey/SurveyRepositoryTest.java
+++ back/src/test/java/com/example/mbti/repository/survey/SurveyRepositoryTest.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -36,5 +38,28 @@ class SurveyRepositoryTest {
         Assertions.assertThat(savePoster.getPosterTitle()).isEqualTo(posterTitle);
         Assertions.assertThat(savePoster.getImgUrl()).isEqualTo(imgUrl);
         Assertions.assertThat(savePoster.getPassage()).isEqualTo(passage);
+    }
+
+    @Test
+    void 제목으로조회(){
+        //given
+        String posterTitle = "심리테스트 유형 제목1";
+        String imgUrl = "심리테스트 유형 주소1";
+        String passage = "심리테스트 유형 지문";
+        Poster poster = Poster.builder()
+                .posterTitle(posterTitle)
+                .imgUrl(imgUrl)
+                .passage(passage)
+                .build();
+        Poster savePoster = posterRepository.save(poster);
+
+        //when
+        Optional<Poster> optPoster = posterRepository.findByTitle(savePoster.getPosterTitle());
+        
+        //then
+        Assertions.assertThat(optPoster.get().getPosterTitle()).isEqualTo(posterTitle);
+        Assertions.assertThat(optPoster.get().getImgUrl()).isEqualTo(imgUrl);
+        Assertions.assertThat(optPoster.get().getPassage()).isEqualTo(passage);
+        Assertions.assertThat(optPoster.get().getPosterId()).isEqualTo(savePoster.getPosterId());
     }
 }
