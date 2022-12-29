4be274b0286e5537a96cc964c7e99511b6ef2f27
DESKTOP-B76L9MB\User <sjs939883@gmail.com>
2022-12-29 16:48:33 +0900

    test: @SpringBoot -> @DataJpaTest로 변경 - JPA 관련 테스트 설정만 로드

--- back/src/test/java/com/example/mbti/repository/CommentRepositoryTest.java
+++ back/src/test/java/com/example/mbti/repository/CommentRepositoryTest.java
@@ -6,6 +6,8 @@ import com.example.mbti.repository.Poster.PosterRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
@@ -13,7 +15,8 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
 
-@SpringBootTest
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class CommentRepositoryTest {
 
     @Autowired
